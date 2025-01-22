4b40e28e850c6794a4a3ae915a8d33e632443da4
Luiz Credidio <lcredidio.dev@gmail.com>
2025-01-21 22:32:01 -0500

    feature: exercise 4 loops

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("  Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int nextNewNumber = -1;
+        float total = 0;
+        int largestNumber = 0;
+        int smallestPositiveNumber = 0;
+        while (nextNewNumber != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            nextNewNumber = int.Parse(Console.ReadLine());
+            if (nextNewNumber != 0)
+            {
+                numbers.Add(nextNewNumber);
+                total += nextNewNumber;
+                if (nextNewNumber > largestNumber)
+                {
+                    largestNumber = nextNewNumber;
+                }
+
+                if (smallestPositiveNumber == 0 & nextNewNumber > 0)
+                {
+                    smallestPositiveNumber = nextNewNumber;
+                }
+
+                if (nextNewNumber > 0 & nextNewNumber < smallestPositiveNumber)
+                {
+                    smallestPositiveNumber = nextNewNumber;
+                }
+            }
+        }
+        float average = (float)total / (float)numbers.Count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
