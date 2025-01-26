57b8c3a576efcb0f51c3d8b1c7230bee64055e26
Luiz Credidio <lcredidio.dev@gmail.com>
2025-01-25 23:48:22 -0500

    Week 2 - Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job firstJob = new Job();
+        firstJob._company = "Progymedia";
+        firstJob._jobTitle = "Full Stack Developer";
+        firstJob._startYear = 2022;
+        firstJob._endYear = 2024;
+
+        Job secondJob = new Job();
+        secondJob._company = "Prelib";
+        secondJob._jobTitle = "Full Stack Developer";
+        secondJob._startYear = 2024;
+        secondJob._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._personName = "Luiz Credidio";
+        resume._jobs.Add(firstJob);
+        resume._jobs.Add(secondJob);
+        resume.DisplayResume();
     }
 }
