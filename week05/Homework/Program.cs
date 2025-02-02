97b23831a11816cbddaa2b4f0b9d7381a5d36255
Luiz Credidio <lcredidio.dev@gmail.com>
2025-02-01 21:39:29 -0500

    Week 5: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+        MathAssignment mathAssignment = new MathAssignment("7.3", "8-19", "Luiz Credidio", "Fractions");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("The Causes of World War II by Mary Waters", "Luiz Credidio", "European History");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
